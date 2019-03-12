d682d6f3568625454832fb20901cf1d334ffa97d
tania <tani.zhelezniak@gmail.com>
2019-03-12 10:16:29 +0200

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PagenewsComponent } from './pagenews/pagenews.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        AppRoutingModule,
+        NativeScriptModule,
+        NativeScriptUISideDrawerModule
+    ],
+    declarations: [
+        AppComponent,
+        PagenewsComponent,
+        MainComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
---
+++ src/app/pagenews/pagenews.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-pagenews',
+  templateUrl: './pagenews.component.html',
+  styleUrls: ['./pagenews.component.css'],
+  moduleId: module.id,
+})
+export class PagenewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
