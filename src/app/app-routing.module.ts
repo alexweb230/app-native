0a744a4052b3ed49e5c2875c5f165c76f8c7763a
tania <tani.zhelezniak@gmail.com>
2019-03-21 16:11:56 +0200

    страница новости

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import {MainComponent} from "~/app/main/main.component";
+import {MainDetailComponent} from "~/app/main-detail/main-detail.component";
 
 const routes: Routes = [
     { path: "", redirectTo: "/main", pathMatch: "full" },
     { path: "main", component: MainComponent },
+    { path: "detail", component: MainDetailComponent },
     { path: "browse", loadChildren: "~/app/browse/browse.module#BrowseModule" },
     { path: "search", loadChildren: "~/app/search/search.module#SearchModule" },
     { path: "featured", loadChildren: "~/app/featured/featured.module#FeaturedModule" },
