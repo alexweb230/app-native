3bb9a5d95eb6ae2e9378c01ba105f200a1eee5fb
tania <tani.zhelezniak@gmail.com>
2019-03-21 16:13:27 +0200

    страница новости

---
+++ src/app/main-detail/main-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-main-detail',
+  templateUrl: './main-detail.component.html',
+  styleUrls: ['./main-detail.component.css'],
+  moduleId: module.id,
+})
+export class MainDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
