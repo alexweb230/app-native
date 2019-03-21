1f7e18afdbc302c3ffd877eba9576f715494e4d6
tania <tani.zhelezniak@gmail.com>
2019-03-21 12:51:54 +0200

    удалил лишние папки

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import {Component, OnInit} from '@angular/core';
+import {RadSideDrawer} from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 
 @Component({
-  selector: 'ns-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css'],
-  moduleId: module.id,
+    selector: 'ns-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css'],
+    moduleId: module.id,
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
     }
+
+    test = 'wwwwwwwwwwww';
+    newsListMain = [
+        {title: 'test-1'},
+        {title: 'test-2'},
+        {title: 'test-3'},
+        {title: 'test-4'},
+        {title: 'test-4'},
+        {title: 'test-4'},
+        {title: 'test-4'},
+        {title: 'test-4'},
+        {title: 'test-4'},
+        {title: 'test-4'},
+    ]
+    newsLis = [
+       'test-1',
+        'test-2',
+        'test-3',
+       'test-4'
+    ]
 }
